b6fff95d1056853e73a3566dcbfc5be196135f64
DongZhiChen <644495218@qq.com>
2017-04-01 04:53:15 +0800

    Initial Commit

---
+++ HouseInfo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HouseInfo
+//
+//  Created by 陈东芝 on 17/4/1.
+//  Copyright © 2017年 陈东芝. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HouseInfo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HouseInfo
+//
+//  Created by 陈东芝 on 17/4/1.
+//  Copyright © 2017年 陈东芝. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
